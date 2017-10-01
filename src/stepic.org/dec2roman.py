421f24478424c0fa80f68000ca6280cc026d5ea4
mickey1cx <mickey1cx@gmail.com>
2017-10-01 20:20:27 +0300

    dec 2 roman

---
+++ src/stepic.org/dec2roman.py
@@ -0,0 +1,7 @@
+num = int(input())
+roman_num = ''
+roman = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M', 900:'CM', 400:'CD', 90:'XC', 40:'XL', 9:'IX', 4:'IV'}
+for l in sorted(roman.keys(), reverse=True):
+    roman_num += str(num // l * roman[l])
+    num = num % l
+print(roman_num)
