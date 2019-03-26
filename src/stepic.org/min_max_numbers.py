7962a17d10a5cf1043ca43359dfa48537ede5aba
mickey1cx <mickey1cx@gmail.com>
2019-03-26 21:41:38 +0300

    https://stepik.org/lesson/5047/step/5?unit=1086

---
+++ src/stepic.org/min_max_numbers.py
@@ -0,0 +1,8 @@
+# python 3.7
+# https://stepik.org/lesson/5047/step/5?unit=1086
+
+numbers = [int(input()), int(input()), int(input())]
+numbers.sort()
+print(numbers[2])
+print(numbers[0])
+print(numbers[1])
