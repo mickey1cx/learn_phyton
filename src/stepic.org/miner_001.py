6803c312e5a44c8c2ec431c9102a4a76fce2798f
mickey1cx <mickey1cx@gmail.com>
2019-04-11 22:33:19 +0300

    miner - add mine counters

--- src/stepic.org/miner_001.py
+++ src/stepic.org/miner_001.py
@@ -13,4 +13,24 @@ field = [[0 for j in range(columns)] for i in range(rows)]
 for mine in mines:
     field[mine[0] - 1][mine[1] - 1] = "*"
 
+for i in range(rows):
+    imin = 0 if i == 0 else -1
+    imax = 0 if i == rows - 1 else 1
+    for j in range(columns):
+
+        if field[i][j] == "*":
+            continue
+
+        jmin = 0 if j == 0 else -1
+        jmax = 0 if j == columns - 1 else 1
+
+        counter = 0
+        for ii in range(imin, imax + 1):
+            for jj in range(jmin, jmax + 1):
+                if ii == 0 and jj == 0:
+                    continue
+                elif field[i + ii][j + jj] == "*":
+                    counter += 1
+        field[i][j] = counter if counter > 0 else "."
+
 print_minefield(field)
