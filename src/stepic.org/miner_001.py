6d30e3860ae13b9183bdf8e5d2a7c27ba34bc6d3
mickey1cx <mickey1cx@gmail.com>
2019-04-11 22:41:22 +0300

    miner - add mine counters

--- src/stepic.org/miner_001.py
+++ src/stepic.org/miner_001.py
@@ -8,7 +8,7 @@ def print_minefield(minefield):
 
 rows, columns, mines = 5, 4, 4  # int(input()), int(input()), int(input())
 mines = [[1, 1], [2, 2], [3, 2], [4, 4]]
-field = [[0 for j in range(columns)] for i in range(rows)]
+field = [[0] * columns for row in range(rows)]  # [[0 for j in range(columns)] for i in range(rows)]
 
 for mine in mines:
     field[mine[0] - 1][mine[1] - 1] = "*"
