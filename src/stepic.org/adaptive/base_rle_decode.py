f688a529d3ec0bfebcc46203a83c3a60ba7ef770
mickey1cx <mickey1cx@gmail.com>
2019-04-25 21:35:50 +0300

    base_rle_decode

---
+++ src/stepic.org/adaptive/base_rle_decode.py
@@ -0,0 +1,21 @@
+# https://stepik.org/lesson/21300/step/2?adaptive=true&unit=5101
+
+import re
+
+data = input()  # '3ab4c2CaB'
+l = re.findall('((\d*)(\w))', data)
+for i in l:
+    print(i[2] * (1 if i[1] == '' else int(i[1])), end='')  # aaabccccCCaB
+
+# 2
+print()
+for full, count, letter in l:
+    print(letter * (1 if count == '' else int(count)), end='')
+
+# 3 !!!
+# ((\d*)(\w)) = group, [0]full, [1]count, [2]letter
+# (\d*)(\w)) = group, [0]count, [1]letter
+l = re.findall('(\d*)(\w)', data)
+print()
+for count, letter in l:
+    print(letter * (1 if count == '' else int(count)), end='')
