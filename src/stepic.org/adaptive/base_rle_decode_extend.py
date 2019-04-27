d3d1be1c1db091fa065032027928006d248d5661
mickey1cx <mickey1cx@gmail.com>
2019-04-27 11:37:48 +0300

    split_decode_series, generator

--- src/stepic.org/adaptive/base_rle_decode_extend.py
+++ src/stepic.org/adaptive/base_rle_decode_extend.py
@@ -5,7 +5,7 @@ import re
 
 def split_decode_series(string):
     for count, letter in re.findall('(\d*)(\w)', string):
-        yield (1 if count == '' else int(count), letter)
+        yield 1 if count == '' else int(count), letter
 
 
 print(list(split_decode_series('2a3bB')))   # [(2, 'a'), (3, 'b'), (1, 'B')]
