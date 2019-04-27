4317e5783fa460180f7b59e5f15f9566de6d76c5
mickey1cx <mickey1cx@gmail.com>
2019-04-27 12:43:50 +0300

    fraction_max_score, collections

--- src/stepic.org/adaptive/fraction_max_score.py
+++ src/stepic.org/adaptive/fraction_max_score.py
@@ -5,3 +5,10 @@ scores = 'F B A A B C A D'  # input()
 score_list = scores.split()
 count_A = score_list.count('A')
 print("{:.2f}".format(count_A / len(score_list), 2))
+
+# 2
+
+import collections
+
+c = collections.Counter(score_list)
+print("%.2f" % (c['A'] / len(score_list)))
