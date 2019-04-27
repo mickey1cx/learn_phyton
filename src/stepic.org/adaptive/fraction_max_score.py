fb61c4e98e63a9a6dbae0de21e6333c0a564ea3e
mickey1cx <mickey1cx@gmail.com>
2019-04-27 12:34:53 +0300

    fraction_max_score

---
+++ src/stepic.org/adaptive/fraction_max_score.py
@@ -0,0 +1,7 @@
+# https://stepik.org/lesson/22774/step/2?adaptive=true&unit=5345
+# counting, format, number-formatting
+
+scores = 'F B A A B C A D'  # input()
+score_list = scores.split()
+count_A = score_list.count('A')
+print("{:.2f}".format(count_A / len(score_list), 2))
