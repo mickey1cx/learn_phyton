6258526e582f5de7c429bad478dbcc82086879d9
mickey1cx <mickey1cx@gmail.com>
2019-04-27 22:47:31 +0300

    a_and_b

---
+++ src/stepic.org/adaptive/a_and_b.py
@@ -0,0 +1,6 @@
+# https://stepik.org/lesson/22772/step/2?adaptive=true&unit=5346
+
+a, b = input(), input()
+print("{0} and {1} sat in the tree.".format(a, b))
+print("{0} had fallen, {1} was stolen.".format(a, b))
+print("What's remaining in the tree?")
